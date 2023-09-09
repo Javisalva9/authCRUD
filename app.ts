003a3f0632570d77509c17cbef46a6a0226b9eeb
javisalva <javisalva9@gmail.com>
2023-09-09 11:18:30 +0200

    now on https

--- app.ts
+++ app.ts
@@ -4,7 +4,9 @@ import express from "express";
 import { connectToDatabase } from "./config/database";
 import { userRouter } from "./routes/user";
 import { productRouter } from "./routes/product";
-const { API_PORT, ALLOWED_ORIGINS } = process.env;
+import fs from 'fs';
+import https from 'https';
+const { API_PORT, ALLOWED_ORIGINS, HTTPS_PORT } = process.env;
 const port = process.env.PORT || API_PORT;
 const origins = ALLOWED_ORIGINS ? ALLOWED_ORIGINS.split(';') : [];
 dotenv.config();
@@ -12,16 +14,27 @@ dotenv.config();
 connectToDatabase()
 	.then(() => {
 		const app = express();
-		
+
 		app.use(cors({ origin: origins }));
 
 		// start the Express server
-		app.listen(port, () => {
-			console.log(`Server running at http://localhost:${port}`);
+		/* 		app.listen(port, () => {
+					console.log(`Server running at http://localhost:${port}`);
+		
+					app.use("/users", userRouter);
+					app.use("/products", productRouter);
+		
+				}); */
+
+
+		https.createServer({
+			cert: fs.readFileSync('certificate.crt'),
+			key: fs.readFileSync('privateKey.key')
+		}, app).listen(HTTPS_PORT, function () {
+			console.log(`Server running at https://localhost:${HTTPS_PORT}`);
 
 			app.use("/users", userRouter);
 			app.use("/products", productRouter);
-
 		});
 
 	})
